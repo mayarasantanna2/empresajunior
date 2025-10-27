da385eb8aac3f11ebadf9cb6896a96d5a2ae73af
mayarasantanna2 <mayaraoli.anna@gmail.com>
2025-10-26 22:34:14 -0300

    perfil e pag aluno

--- template/perfilaluno.php
+++ template/perfilaluno.php
@@ -2,222 +2,300 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <title>Perfil - Freetecs</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Perfil - Freetecs</title>
 
-  <!-- Bootstrap -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-  <link rel="stylesheet" href="css/style.css" />
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 </head>
 
 <body>
 
-  <!-- Header -->
-   <header class="header_section">
-    <div class="container">
-      <nav class="navbar navbar-expand-lg custom_nav-container ">
-        <a class="navbar-brand" href="restritaaluno.php">
-          <span>
-            <h2>Olá, Aluno</h2>
-          </span>
-        </a>
+    <!-- Header -->
+    <header class="header_section">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg custom_nav-container ">
+                <a class="navbar-brand" href="restritaaluno.php">
+                    <span>
+                        <h2>Olá, Aluno</h2>
+                    </span>
+                </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class=""> </span>
-        </button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class=""> </span>
+                </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav  mx-auto ">
-            <li class="nav-item ">
-              <a class="nav-link" href="index.html">Início </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="restritaaluno.php">Projetos</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contato.html">Contato</a>
-            </li>
-          </ul>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav  mx-auto ">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="index.html">Início </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="restritaaluno.php">Projetos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contato.html">Contato</a>
+                        </li>
+                    </ul>
+                </div>
         </div>
-    </div>
-    </nav>
-    </div>
-  </header>
-  <!-- end header -->
+        </nav>
+        </div>
+    </header>
+    <!-- end header -->
 
 
-  
-  <!-- Perfil Section -->
-  <section class="food_section layout_padding-bottom">
-    <div class="container">
-      <div class="heading_container heading_center">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <h2>Seu Perfil</h2>
-      </div>
-<br>
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="box p-4" style="text-align: left;">
-            <div class="detail-box">
-              <h5>Informações Públicas</h5>
-              <br>
-              <strong class="tituloperfil" >Nome Completo:</strong>
-              <p>João da Silva Santos</p>
 
-              <strong>E-mail:</strong>
-              <p> joaosilva@email.com</p>
-              
-              <strong>Telefone:</strong>
-              <p>(11) 98765-4321</p>
+    <!-- Perfil Section -->
+    <section class="food_section layout_padding-bottom">
+        <div class="container">
+            <div class="heading_container heading_center">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <h2>Seu Perfil</h2>
+            </div>
+            <br>
 
-              <strong>CPF:</strong>
-              <p>155.584.634-01</p>
 
-              <strong>Endereço:</strong>
-              <p>Rua Jardim, 123 - São Paulo/SP</p>
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="box p-4" style="text-align: left;">
+                        <div class="detail-box">
+                            <h5>Informações Públicas</h5>
+                            <br>
+                            <div class="profile-header">
+                                <div class="profile-pic-container">
+                                    <img src="images/default-profile.png" alt="Foto de perfil" class="profile-pic"
+                                        id="profileImage">
 
-              <div class="btn-box">
-                  <a href="editaraluno.php" class="btn1">
-                    Editar
-                  </a>
+
+                                    <input type="file" id="profileUpload" accept="image/*">
+                                    <label for="profileUpload" class="upload-icon">
+                                        <i class="fa fa-camera"></i>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <strong class="tituloperfil">Nome Completo:</strong>
+                            <p>João da Silva Santos</p>
+
+                            <strong>E-mail:</strong>
+                            <p> joaosilva@email.com</p>
+
+                            <strong>Telefone:</strong>
+                            <p>(11) 98765-4321</p>
+
+                            <strong>Curso:</strong>
+                            <p>Informática Para Internet</p>
+
+                            <strong>Descrição Pessoal:</strong>
+                            <p>Olá! Meu nome é João, tenho 15 anos e curso o 1º ano do Ensino Médio integrado ao técnico
+                                em Informática para Internet na Etec MCM. Moro em Ribeirão Pires com meus pais e minha
+                                gata. Sou apaixonado por música e livros, e gosto de aprender coisas novas,
+                                especialmente na área de tecnologia e design. Busco oportunidades para desenvolver
+                                minhas habilidades, colaborar com projetos criativos e ganhar experiência prática.
+                            </p>
+
+                            <strong>Habilidades e Competências</strong>
+                            <p>Inglês intermediário; HTML básico; CSS básico; JavaScript básico; Word básico; Excel
+                                básico; Photoshop básico; Pontualidade; Criatividade; Trabalho em Equipe.</p>
+                            <br>
+                            <hr class="linha">
+                            <br>
+                            <h5>Informações Privadas</h5>
+                            <br>
+                            <strong class="tituloperfil">Nome Completo:</strong>
+                            <p>João da Silva Santos</p>
+
+                            <strong>E-mail:</strong>
+                            <p> joaosilva@email.com</p>
+
+                            <strong>Telefone:</strong>
+                            <p>(11) 98765-4321</p>
+
+                            <strong>Data de Nascimento:</strong>
+                            <p>123456</p>
+
+                            <strong>CPF:</strong>
+                            <p>155.584.634-01</p>
+
+                            <strong>Registro de Matrícula:</strong>
+                            <p>23678</p>
+
+                            <strong>Senha:</strong>
+                            <p>123456</p>
+
+                            <div class="btn-box">
+                                <a href="editaraluno.php" class="btn1">
+                                    Editar
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
+
         </div>
-      </div>
+    </section>
 
-    </div>
-  </section>
+    <section class="food_section layout_padding-bottom">
+        <div class="container">
+            <div class="heading_container heading_center">
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="box p-4" style="text-align: left;">
+                        <div class="detail-box">
+                            <h5>Informações Privadas</h5>
+                            <br>
+                            <strong class="tituloperfil">Nome Completo:</strong>
+                            <p>João da Silva Santos</p>
 
-   <section class="food_section layout_padding-bottom">
-    <div class="container">
-      <div class="heading_container heading_center">
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="box p-4" style="text-align: left;">
-            <div class="detail-box">
-              <h5>Informações Privadas</h5>
-              <br>
-              <strong class="tituloperfil" >Nome Completo:</strong>
-              <p>João da Silva Santos</p>
+                            <strong>E-mail:</strong>
+                            <p> joaosilva@email.com</p>
 
-              <strong>E-mail:</strong>
-              <p> joaosilva@email.com</p>
-              
-              <strong>Telefone:</strong>
-              <p>(11) 98765-4321</p>
+                            <strong>Telefone:</strong>
+                            <p>(11) 98765-4321</p>
 
-              <strong>CPF:</strong>
-              <p>155.584.634-01</p>
+                            <strong>Data de Nascimento:</strong>
+                            <p>123456</p>
 
-              <strong>Endereço:</strong>
-              <p>Rua Jardim, 123 - São Paulo/SP</p>
+                            <strong>CPF:</strong>
+                            <p>155.584.634-01</p>
 
-              <div class="btn-box">
-                  <a href="editaraluno.php" class="btn1">
-                    Editar
-                  </a>
+                            <strong>Registro de Matrícula:</strong>
+                            <p>123456</p>
+
+                            <strong>Senha:</strong>
+                            <p>123456</p>
+
+                            <strong>Senha:</strong>
+                            <p>123456</p>
+                            <div class="btn-box">
+                                <a href="editaraluno.php" class="btn1">
+                                    Editar
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-    </div>
-  </section>
-  <!-- end Perfil Section -->
+        </div>
+    </section>
+    <!-- end Perfil Section -->
 
-  <!-- Footer -->
-  <footer class="footer_section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 footer-col">
-          <div class="footer_contact">
-            <h4>
-              Contact Us
-            </h4>
-            <div class="contact_link_box">
-              <a href="">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span>
-                  Location
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-                <span>
-                  Call +01 1234567890
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <span>
-                  demo@gmail.com
-                </span>
-              </a>
+    <!-- Footer -->
+    <footer class="footer_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 footer-col">
+                    <div class="footer_contact">
+                        <h4>
+                            Contact Us
+                        </h4>
+                        <div class="contact_link_box">
+                            <a href="">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                <span>
+                                    Location
+                                </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <span>
+                                    Call +01 1234567890
+                                </span>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                                <span>
+                                    demo@gmail.com
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <div class="footer_detail">
+                        <a href="" class="footer-logo">
+                            Feane
+                        </a>
+                        <p>
+                            Necessary, making this the first true generator on the Internet. It uses a dictionary of
+                            over 200 Latin
+                            words, combined with
+                        </p>
+                        <div class="footer_social">
+                            <a href="">
+                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                            <a href="">
+                                <i class="fa fa-pinterest" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 footer-col">
+                    <h4>
+                        Opening Hours
+                    </h4>
+                    <p>
+                        Everyday
+                    </p>
+                    <p>
+                        10.00 Am -10.00 Pm
+                    </p>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4 footer-col">
-          <div class="footer_detail">
-            <a href="" class="footer-logo">
-              Feane
-            </a>
-            <p>
-              Necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin
-              words, combined with
-            </p>
-            <div class="footer_social">
-              <a href="">
-                <i class="fa fa-facebook" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-linkedin" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-instagram" aria-hidden="true"></i>
-              </a>
-              <a href="">
-                <i class="fa fa-pinterest" aria-hidden="true"></i>
-              </a>
+            <div class="footer-info">
+                <p>
+                    &copy; <span id="displayYear"></span> All Rights Reserved By
+                    <a href="https://html.design/">Free Html Templates</a><br><br>
+                    &copy; <span id="displayYear"></span> Distributed By
+                    <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+                </p>
             </div>
-          </div>
         </div>
-        <div class="col-md-4 footer-col">
-          <h4>
-            Opening Hours
-          </h4>
-          <p>
-            Everyday
-          </p>
-          <p>
-            10.00 Am -10.00 Pm
-          </p>
-        </div>
-      </div>
-      <div class="footer-info">
-        <p>
-          &copy; <span id="displayYear"></span> All Rights Reserved By
-          <a href="https://html.design/">Free Html Templates</a><br><br>
-          &copy; <span id="displayYear"></span> Distributed By
-          <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
-        </p>
-      </div>
-    </div>
-  </footer>
-  <!-- Scripts -->
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <script src="js/bootstrap.js"></script>
+    </footer>
+    <!-- Scripts -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script>
+        const uploadInput = document.getElementById('profileUpload');
+        const profileImage = document.getElementById('profileImage');
+
+        uploadInput.addEventListener('change', function () {
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    profileImage.src = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+    </script>
+
+
 </body>
 
 </html>
