b6eecd295e72c4d3c9977f9867147b269a2b2e9e
rillopedro <pedrorillo08@gmail.com>
2025-11-24 08:35:14 -0300

    apresentação

--- template/headeraluno.php
+++ template/headeraluno.php
@@ -1,16 +1,8 @@
-<!-- header section strats -->
-  <?php
-    require 'conexao.php';
-  ?>
-  <header class="header_section">
-    <div class="container">
-      <nav class="navbar navbar-expand-lg custom_nav-container ">
-        <a class="navbar-brand" href="restritaaluno.php">
-           <span>
-           <?php
+<?php
 session_start();
 require 'conexao.php';
 
+// Se não estiver logado, redireciona
 if (!isset($_SESSION['id'])) {
     header("Location: login.php");
     exit;
@@ -18,32 +10,41 @@ if (!isset($_SESSION['id'])) {
 
 $id = $_SESSION['id'];
 
-$sql = "SELECT nome_aluno FROM aluno WHERE id_aluno = :id";
+// Busca o nome do aluno logado
+$sql = "SELECT nome_aluno FROM aluno WHERE id_aluno = :id LIMIT 1";
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':id', $id);
 $stmt->execute();
 $aluno = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
-          </span>
-        </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class=""> </span>
-        </button>
+<!-- header section -->
+<header class="header_section">
+  <div class="container">
+    <nav class="navbar navbar-expand-lg custom_nav-container">
+      
+      <a class="navbar-brand" href="restritaaluno.php">
+        <span>
+          Olá, <?php echo htmlspecialchars($aluno['nome_aluno']); ?>
+        </span>
+      </a>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav  mx-auto ">
-            <li class="nav-item ">
-              <a class="nav-link" href="index.php">Início </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="perfilaluno.php">Seu Perfil</a>
-            </li>
-          </ul>
-        </div>
-    </div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class=""></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="restritaaluno.php">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="perfilaluno.php">Seu Perfil</a>
+          </li>
+        </ul>
+      </div>
+
     </nav>
-    </div>
-  </header>
-  <!-- end header section -->
+  </div>
+</header>
